fe1b7e69a991178cf67764cc2b3a872af1b598c9
Selina Lavery <selina118@hotmail.com>
2023-03-05 22:25:15 +0000

    Generated a sessions controller, updated the routes, the controller and a new file for user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+ #   render :new login form
+    
+  end
+
+  def create
+ #authenticate user and log them in
+  end
+
+  def destroy
+ #log user out
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
