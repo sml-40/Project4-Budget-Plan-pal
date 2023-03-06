794db2b75707dd521992fc65a4bd0bc5448d3908
Selina Lavery <selina118@hotmail.com>
2023-03-06 14:53:44 +0000

    Add seed data for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(first_name: "Marc", last_name: "Lavery", email: "marc@email.com", username: "marc", password: "pass1234")
+User.create(first_name: "John", last_name: "Doe", email: "john@email.com", username: "john", password: "pass1234")
+User.create(first_name: "Jane", last_name: "Doe", email: "jane@email.com", username: "jane", password: "pass1234")
+User.create(first_name: "Bob", last_name: "Smith", email: "bob@email.com", username: "bob", password: "pass1234")
+User.create(first_name: "Sally", last_name: "Jones", email: "sally@email.com", username: "sally", password: "pass1234")
+
