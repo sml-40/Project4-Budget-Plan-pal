1d8e92fa21c87730940d10eea3277ebfeeace299
Selina Lavery <selina118@hotmail.com>
2023-03-07 12:34:10 +0000

    Updated categories controller with new and create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,20 @@ class CategoriesController < ApplicationController
   def index
     #Calling the model to get the data from the database.
     # makes the categories available to the view, so we can iterate over them, class instance variable.
-    @categories = current_user.categories
+    @categories = Category.all
+  end
+
+  def create
+    # Create a new category using the data from the form
+    category = Category.new
+    category.name = params[:category][:name]
+    category.save
+    # Redirect to the categories index page
+    redirect_to categories_path
+  end
+
+  def new
+    # Create a new category object
+    @category = Category.new
   end
 end
