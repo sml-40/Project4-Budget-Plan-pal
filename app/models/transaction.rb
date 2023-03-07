3127816df8c38987f0d0d4919757b41fdb958d32
Selina Lavery <selina118@hotmail.com>
2023-03-07 15:16:08 +0000

    Add edit file for the edit form. Also defined a constant CATEGORIES in your Transaction model that contains an array of all possible transaction categories.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
+  CATEGORIES = ["Groceries", "Travel", "Food", "Entertainment", "Housing", "Utilities" "Transportation", "Personal Care", "Education", "Health/fitness", "Miscellaneous"]
   belongs_to :user
 
   validates :amount, presence: true
